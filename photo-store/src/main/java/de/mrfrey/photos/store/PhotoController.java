092179d92aef7c6f51bdcc9d129697b816515041
Rainer Frey <frey.rainer@gmail.com>
2016-12-02 16:35:51 +0100

    Add photo REST endpoint

--- photo-store/src/main/java/de/mrfrey/photos/store/PhotoController.java
+++ photo-store/src/main/java/de/mrfrey/photos/store/PhotoController.java
@@ -1,6 +1,7 @@
 package de.mrfrey.photos.store;
 
 import org.bson.types.ObjectId;
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +9,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @Controller
 @RequestMapping("/photos")
@@ -19,9 +24,17 @@ public class PhotoController {
         this.photoStorageService = photoStorageService;
     }
 
+    @GetMapping("/{id}")
+    @ResponseBody
+    public Resource<Photo> get(@PathVariable("id") ObjectId id) {
+        Link self = linkTo(methodOn(PhotoController.class).get(id)).withSelfRel();
+        Link image = linkTo(methodOn(ImageController.class).getImage(id)).withRel("image:original");
+        return new Resource<>(photoStorageService.getPhoto(id), self, image);
+    }
+
     @PostMapping
-    public String upload(@RequestPart("image-file")MultipartFile imageFile) {
+    public String upload(@RequestPart("image-file") MultipartFile imageFile) {
         photoStorageService.storePhoto(imageFile);
-        return "redirect:uploadSuccess";
+        return "redirect:upload.html";
     }
 }
--- photo-store/src/main/java/de/mrfrey/photos/store/PhotoStoreApplication.java
+++ photo-store/src/main/java/de/mrfrey/photos/store/PhotoStoreApplication.java
@@ -1,5 +1,16 @@
 package de.mrfrey.photos.store;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import org.bson.types.ObjectId;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,12 +19,8 @@ import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Processor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.integration.dsl.IntegrationFlow;
-import org.springframework.integration.dsl.IntegrationFlows;
 
-import java.util.Map;
-
-import static org.springframework.integration.dsl.support.Transformers.fromJson;
+import java.io.IOException;
 
 @SpringBootApplication
 @EnableBinding(Processor.class)
@@ -28,14 +35,34 @@ public class PhotoStoreApplication {
         return new AlwaysSampler();
     }
 
-//    @Bean
-//    public IntegrationFlow addMetadata(Processor processor, PhotoStorageService photoStorageService) {
-//        return IntegrationFlows
-//                .from(processor.input())
-////                .transform(fromJson(Metadata.class))
-//                .handle(Map.class, (payload, headers) -> {
-//                    photoStorageService.addMetadata((String) headers.get("photo-id"), payload);
-//                    return null;
-//                }).get();
-//    }
+    @Bean
+    Module objectIdModule() {
+        SimpleModule module = new SimpleModule("ObjectId");
+        module.addSerializer(ObjectId.class, new ObjectIdSerializer());
+        module.addDeserializer(ObjectId.class, new ObjectIdDeserializer());
+        return module;
+    }
+
+}
+
+class ObjectIdDeserializer extends JsonDeserializer<ObjectId> {
+
+    @Override
+    public ObjectId deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+
+        JsonNode node = p.getCodec().readTree(p);
+        return new ObjectId(node.textValue());
+    }
+}
+
+class ObjectIdSerializer extends JsonSerializer<ObjectId> {
+
+    @Override
+    public void serialize(ObjectId value, JsonGenerator gen, SerializerProvider serializers) throws IOException, JsonProcessingException {
+        if (value == null) {
+            gen.writeNull();
+        } else {
+            gen.writeString(value.toString());
+        }
+    }
 }
