90f61a79f0bf17da06204c998ad06b6875421f4a
Rainer Frey <frey.rainer@gmail.com>
2017-02-07 07:08:22 +0100

    Upgrade to Spring Boot 1.5 and Spring Cloud SR5

--- photo-store/src/main/java/de/mrfrey/photos/store/web/RootController.java
+++ photo-store/src/main/java/de/mrfrey/photos/store/web/RootController.java
@@ -1,7 +1,7 @@
 package de.mrfrey.photos.store.web;
 
-import de.escalon.hypermedia.affordance.Affordance;
 import de.mrfrey.photos.store.Photo;
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.ResourceSupport;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 
-import static de.escalon.hypermedia.spring.AffordanceBuilder.linkTo;
-import static de.escalon.hypermedia.spring.AffordanceBuilder.methodOn;
 import static org.springframework.hateoas.MediaTypes.HAL_JSON_VALUE;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @RequestMapping(value = "/", produces = HAL_JSON_VALUE)
 @RestController
@@ -19,17 +19,17 @@ public class RootController {
 
     @GetMapping
     public ResourceSupport root() {
-        Affordance self = linkTo(RootController.class).withSelfRel();
-        Affordance photos = linkTo(methodOn(PhotoController.class).allPhotos()).withRel("photos");
-        Affordance photo = linkTo(methodOn(PhotoController.class).get(null)).withRel("photo");
-        Affordance uploadPhoto = linkTo(PhotoController.class).withRel("photo:upload");
+        Link self = linkTo(RootController.class).withSelfRel();
+        Link photos = linkTo(methodOn(PhotoController.class).allPhotos()).withRel("photos");
+        Link photo = linkTo(methodOn(PhotoController.class).get(null)).withRel("photo");
+        Link uploadPhoto = linkTo(PhotoController.class).withRel("photo:upload");
 
-        Affordance original = linkTo(methodOn(ImageController.class).getImage(null, Photo.Size.original)).withRel("image:original");
-        Affordance scaled = linkTo(methodOn(ImageController.class).getImage(null, Photo.Size.scaled)).withRel("image:scaled");
-        Affordance thumb = linkTo(methodOn(ImageController.class).getImage(null, Photo.Size.thumbnail)).withRel("image:thumbnail");
-        Affordance image = linkTo(methodOn(ImageController.class).getImage(null, null)).withRel("image");
-        Affordance uploadImage = linkTo(methodOn(ImageController.class).uploadScaledImage(null, null, Photo.Size.scaled)).withRel("image:upload:scaled");
-        Affordance uploadThumb = linkTo(methodOn(ImageController.class).uploadScaledImage(null, null, Photo.Size.thumbnail)).withRel("image:upload:thumbnail");
+        Link original = linkTo(methodOn(ImageController.class).getImage(null, Photo.Size.original)).withRel("image:original");
+        Link scaled = linkTo(methodOn(ImageController.class).getImage(null, Photo.Size.scaled)).withRel("image:scaled");
+        Link thumb = linkTo(methodOn(ImageController.class).getImage(null, Photo.Size.thumbnail)).withRel("image:thumbnail");
+        Link image = linkTo(methodOn(ImageController.class).getImage(null, null)).withRel("image");
+        Link uploadImage = linkTo(methodOn(ImageController.class).uploadScaledImage(null, null, Photo.Size.scaled)).withRel("image:upload:scaled");
+        Link uploadThumb = linkTo(methodOn(ImageController.class).uploadScaledImage(null, null, Photo.Size.thumbnail)).withRel("image:upload:thumbnail");
 
         ResourceSupport resource = new ResourceSupport();
         resource.add(self, photos, photo, uploadPhoto, original, scaled, thumb, image, uploadImage, uploadThumb);
@@ -38,6 +38,6 @@ public class RootController {
 
     @GetMapping("/user")
     public Principal user(Principal principal) {
-        return  principal;
+        return principal;
     }
 }
