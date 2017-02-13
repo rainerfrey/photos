4d49fc3843b7ddb4c7c663daa4860087de14b1b5
Rainer Frey <frey.rainer@gmail.com>
2017-02-13 08:06:01 +0100

    Upgrade ember, refresh index page, refactor index template

--- photo-ui/app/routes/index.js
+++ photo-ui/app/routes/index.js
@@ -2,8 +2,16 @@ import Ember from "ember";
 import AuthenticatedRouteMixin from "ember-simple-auth/mixins/authenticated-route-mixin";
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  photoUpdates: Ember.inject.service(),
+
+  init() {
+    this._super(...arguments);
+    this.get("photoUpdates").on("newPhoto", ()=>{
+      this.refresh();
+    });
+  },
 
   model() {
-    return this.get("store").findAll("photo");
+    return this.get("store").findAll("photo").then((photos)=>{return photos.get("lastObject")});
   }
 });
--- photo-ui/app/services/photo-updates.js
+++ photo-ui/app/services/photo-updates.js
@@ -3,26 +3,37 @@ import Update from "photo-ui/models/update";
 
 const MAX_UPDATES = 10;
 
-export default Ember.Service.extend({
+export default Ember.Service.extend(Ember.Evented, {
   stomp: Ember.inject.service(),
   events: Ember.inject.service(),
   newPhotos: null,
   myUpdates: null,
-    myCount: null,
+  myCount: null,
 
   init() {
     this._super(...arguments);
     this.set("newPhotos", []);
     this.set("myUpdates", []);
     this.get("events").on("stompStarted", () => {
-      this.get("stomp").subscribe("/app/count", (m)=> {this.set("myCount", m.count);}, this);
-      this.get("stomp").subscribe("/topic/photos", (m)=>{this.onMessage(m,this.get("newPhotos"));}, this);
-      this.get("stomp").subscribe("/user/exchange/amq.direct/photo.update", (m)=>{this.onMessage(m,this.get("myUpdates"));}, this);
+      this.get("stomp").subscribe("/app/count", (m) => {
+        this.set("myCount", m.count);
+      }, this);
+      this.get("stomp").subscribe("/topic/photos", this.newPhoto, this);
+      this.get("stomp").subscribe("/user/exchange/amq.direct/photo.update", this.photoUpdate, this);
       this.get("stomp").subscribe("/user/exchange/amq.direct/errors", this.onError, this);
     });
   },
 
-  onMessage(message, target) {
+  newPhoto(message) {
+    this.updateList(message, this.get("newPhotos"));
+    this.trigger("newPhoto");
+  },
+
+  photoUpdate(message) {
+    this.updateList(message, this.get("myUpdates"));
+  },
+
+  updateList(message, target) {
     target.pushObject(Update.create(message));
     if (target.get("length") > MAX_UPDATES) {
       target.shiftObject();
