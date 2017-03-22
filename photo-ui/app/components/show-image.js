cebbaf1748d7e0a146beb15e10d3f6df939f7218
Rainer Frey <frey.rainer@gmail.com>
2017-03-22 07:17:07 +0100

    Improve image caption display

--- photo-ui/app/components/show-image.js
+++ photo-ui/app/components/show-image.js
@@ -2,13 +2,13 @@ import Ember from "ember";
 const {computed, isPresent} = Ember;
 
 export default Ember.Component.extend({
-    caption: computed("photo.caption", function () {
+    caption: computed("photo.caption", "photo.fileName", function () {
         let caption = this.get("photo.caption");
         if (isPresent(caption)) {
             return caption;
         }
         else {
-            return "";
+            return this.get("photo.fileName");
         }
     })
 });
