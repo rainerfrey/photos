3ebb1526a1e47072a8a14b4029c12d42e73a4296
Rainer Frey <frey.rainer@gmail.com>
2017-09-10 15:52:44 +0200

    Prevent event bubbling (and thus the reload) on login form submit, fixing the double login problem

--- photo-ui/app/components/login-form.js
+++ photo-ui/app/components/login-form.js
@@ -1,7 +1,13 @@
 import Ember from "ember";
 
 export default Ember.Component.extend({
-    classNames: ["xc_login-page"],
+    classNames: ["xc_login-page", "xc_login-form"],
+    tagName: "form",
     username: null,
-    password: null
+    password: null,
+
+    submit() {
+        this.get("loginAction")(this.get("username"), this.get("password"));
+        return false;
+    }
 });
--- photo-ui/app/routes/login.js
+++ photo-ui/app/routes/login.js
@@ -6,12 +6,14 @@ export default Ember.Route.extend(UnauthenticatedRouteMixin, {
     events: Ember.inject.service(),
     actions: {
         login: function (username, password) {
-            this.get("session").authenticate("authenticator:oauth2", username, password, "photo-ui").then(() => {
+            Ember.Logger.info("Logging in");
+            return this.get("session").authenticate("authenticator:oauth2", username, password, "photo-ui").then(() => {
                 Ember.Logger.info("Logged in");
                 Ember.Logger.info(JSON.stringify(this.get("session.data")));
                 this.get("events").trigger("loggedIn");
-            }, (error) => {
+            }).catch( (error) => {
                 Ember.Logger.error(JSON.stringify(error));
+                throw error;
             });
         }
     }
