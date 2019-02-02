534fa75395c0c8ca7e68f5287bed662b0443f369
Rainer Frey <frey.rainer@gmail.com>
2019-02-03 00:34:22 +0100

    Ember 3.7.1

--- photo-ui/.eslintrc.js
+++ photo-ui/.eslintrc.js
@@ -21,8 +21,11 @@ module.exports = {
     // node files
     {
       files: [
-        'testem.js',
+        '.eslintrc.js',
+        '.template-lintrc.js',
         'ember-cli-build.js',
+        'testem.js',
+        'blueprints/*/index.js',
         'config/**/*.js',
         'lib/*/index.js'
       ],
@@ -33,15 +36,6 @@ module.exports = {
       env: {
         browser: false,
         node: true
-      }
-    },
-
-    // test files
-    {
-      files: ['tests/**/*.js'],
-      excludedFiles: ['tests/dummy/**/*.js'],
-      env: {
-        embertest: true
       }
     }
   ]
